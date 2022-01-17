8e5ba2579c76c81a64f659042f00028aad211d37
Dines <dinesvlarsen@gmail.com>
2022-01-17 17:53:23 +0100

    Added main articles to the routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import Home from './views/Home.vue';
+import MainArticles from './views/MainArticles.vue';
 
 export default [
-	{ name: 'home', path: '/', component: Home }
+	{ name: 'home', path: '/', component: Home },
+	{ name: 'mainarticles', path: '/mainarticles', props: true, component: MainArticles },
 ];
