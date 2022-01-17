b2ba5036e7d02ea6864e5a4e461ab0e73795b7e6
Dines <dinesvlarsen@gmail.com>
2022-01-17 17:52:53 +0100

    grouped all imports together

--- public/assets/database.js
+++ public/assets/database.js
@@ -23,7 +23,7 @@ export default {
 		{
 			title: 'The Institute of the Cosmos',
 			slug: 'the-institute-of-the-cosmos',
-			lead: 'The Institute of the Cosmos is an ongoing collective research project founded in 2019. Informed by the historical ideas of Russian Cosmism, the Institute is a space for a creative investigation of the materiality of the cosmos and its strange universalism, from the perspectives of philosophy, anthropology, history of science, and art.'
+			lead: 'The Institute of the Cosmos is an ongoing collective research project founded in 2019. Informed by the historical ideas of Russian Cosmism, the Institute is a space for a creative investigation of the materiality of the cosmos and its strange universalism, from the perspectives of philosophy, anthropology, history of science, and art.',
 			body: [
 				`The Polish magazine Obieg (Circuit) was launched in 1987, beginning as a bulletin; its name was a reference to a “third circuit” of publications that the magazine itself attempted to forge: autonomous from the dictates of the state, yet also distinct from the publications produced by the “official” political opposition, including the Solidarity movement and associated groups. The publication soon became one of the only periodicals in Polish to offer space for alternative communities and ideas; quickly, it grew into a trusted information guide and platform for exchanging what would become crucial historical perspectives and concepts. As Poland underwent seismic political changes, so did the magazine. In the 1990s, it was no longer a bulletin, but rather transformed into a fully fledged publication funded by the Ujazdowski Castle Centre for Contemporary Art (CCA Ujazdowski). Briefly suspended in 2002, the magazine resumed online in 2004. Notwithstanding that after a decade and a half it became aligned with a museum—a state institution after all—it retained autonomy to publish what it had always pursued.`,
 				`I was the editor of Obieg from January 2016 until July 2020. On my watch, Obieg stopped exclusively publishing reviews of the Polish art scene (2004–2015), to the dismay of the Warsaw art world. But we were adamant: our aim was to connect with the international world of art, to enrich the local scene rather than function merely as a self-serving echo chamber of a publicly funded exhibition venue, with the “usual suspects” tooting their own horns. The vast majority of Obieg ’s readers from Poland and beyond were art professionals, students in their early twenties, and others appreciative of our longer reads.`,
@@ -32,7 +32,7 @@ export default {
 				`Despite our meagre budget, we managed to increase our fees for authors—regardless of their celebrity status or passport—as well as the fees for translators, guest editors, and all freelancers involved, to increase our reach and international community. Because of this, I believe, we were successful. After writing an essay for Obieg, someone from Lagos was granted a stipend in Johannesburg. Someone else came up with the idea of a biennial in northern Chile. New projects were conceived in Kyiv, Rijeka, Vilnius, and many other places. Channeled through Warsaw, people and ideas connected in fruitful collaboration.`,
 			],
 			preview: {
-				image: 'article_1.jpg',
+				image: 'article_1.jpeg',
 				caption: `Liz Craft, She is looking at me, 2015. Ceramic, aluminium, 84 x 82 x 12 cm.`
 			},
 		},
@@ -47,7 +47,7 @@ export default {
 				`The back gallery showcases imPOSSIBLE Conversations, a 2013 installation-based work that carries the thread of these early experiments forward into work made before Rapoport’s death in 2015 and expands their possibility for manipulation and iteration by adding a social dimension. Drawing on Alvin Roth and Lloyd Shapley’s economic theories about how audiences, institutions, and companies form stable matches within markets, Rapoport made black-and-white, 8 x 10” photographs of a selection of these early fabric paintings, and then paired each with an advertisement page from editions of The New York Times to form a collaged, composite artwork. She then invited ten viewers to assign a headline from other advertisements to the ten artworks, installing the new headlines directly above the composites. The resulting pairings are uncanny: both in the way her paintings echo the forms in the advertisements, and in how the viewers assign content and even humor to the visual information. In this work, as across this show, Rapoport is an artist-engineer, coding and riffing on the internal structures of her artworks to create new visual data for the viewer.`,
 			],
 			preview: {
-				image: 'article_2.jpg',
+				image: 'article_2.jpeg',
 				caption: ''
 			}
 		},
@@ -59,10 +59,10 @@ export default {
 				`In my 2016 article “Algo-ritmo,” I hyphenated the Spanish word for algorithm, creating a portmanteau consisting of the words “something” and “rhythm.”1 I played off the word “rhythm” to speak to what Derrida calls iterability in speech acts. Derrida argues that what makes speech acts effective are their iterability, that is, their repetition with alterity. For Derrida, it is alterity that enables conditions of possibility as opposed to determination—conditions of play. What McKittrick calls for in “Mathematics Black Life” is an alternative reading of the enumeration of colonial and anti-Black violence that renders an other-wise2 understanding, one that is based on an onto-epistemology that accounts for Black survival and those who lived.`,
 				`McKittrick’s focus on the “contours of Black life” as well as my focus on the “ritmo” of algo-ritmo imply patterns or rhythms that I would like to focus on here. I will argue that these patterns or rhythms are where the art or poethics of quantification lie. This is particularly important given that I will argue that algorithmic governance is a process of recursive rhythms and patterns. These rhythms and patterns are what preemptively shape a racializing affect, or what Jasbir Puar calls “slow life and debility.”3 I argue that algorithmic governance is made up of a system of modulating diffractive mechanisms (or diffractive modulators) that seek to compress the information patterns and rhythms of the world. Here, I see the potential of a poethics of other-wise diffractive patterns toward rerouting the onto-epistemology of the recursive system of algorithmic governance. I turn to algorithmic governance first.`,
 				`Since World War II, societies have been shifting from systems of institutional enclosures that discipline citizens’ ways of being to systems of infinite and continuous modulating mechanisms, which generatively control access to institutions and human behavior.4 Digital technologies and the “internet of things” have enabled increasingly distributed logics, rationalities, and practices of governance via cybernetic systems of communication and predictive control. While sovereign, discipline, and control technologies of capture continue to exist in concert, it is increasingly control that becomes the dominant logic of systems of governance.`,
-				`Within this context of cybernetic systems of governance, control has become the guaranteed form of “truth.” That is, the assured path to “truth” is to shape the futurity of “truth” in the present. Thus, rather than try to prevent or deter the existence of what is empirically verifiable, cybernetic systems of control work via a play on temporality that manufactures a becoming-assemblage such as an event, organization, or body in the form of a threat, an anxiety, or desire to mold futurity in the present (or present futurity). In other words, regardless of whether a becoming-assemblage is empirically or objectively verifiable, a future event is constructed based on already existing predispositions or beliefs. The operative logic for such an environment of manufactured futurity is preemptive action.`,
+				`Within this context of cybernetic systems of governance, control has become the guaranteed form of “truth.” That is, the assured path to “truth” is to shape the futurity of “truth” in the present. Thus, rather than try to prevent or deter the existence of what is empirically verifiable, cybernetic systems of control work via a play on temporality that manufactures a becoming-assemblage such as an event, organization,  in the form of a threat, an anxiety, or desire to mold futurity in the present (or present futurity). In other words, regardless of whether a becoming-assemblage is empirically or objectively verifiable, a future event is constructed based on already existing predispositions or beliefs. The operative logic for such an environment of manufactured futurity is preemptive action.`,
 			],
 			preview: {
-				image: 'article_3.jpg'
+				image: 'article_3.jpeg',
 				caption: `Cover of Obieg 12: “The Speed of Guccifer.” The studio of TV UNU in the Romanian city of Râmnicu Vâlcea (a.k.a. Hackerville). Photo: Nicoleta Moise.`
 			},
 		},
@@ -72,33 +72,33 @@ export default {
 		{
 			title: 'Radicalizing Care',
 			writers: 'Birgit Bosold, Lena Fritsch, Vera Hofmann, Elke Krasny, Sophie Lingg',
-			cover: 'book_1.jpg'
+			cover: 'book_1.jpeg'
 		},
 		{
 			title: 'Curating the Complex & The Open Strike',
 			writers: 'TERRY SMITH',
-			cover: 'book_2.jpg'
+			cover: 'book_2.jpeg'
 		},
 		{
 			title: 'Amazonia',
 			writers: 'KATERYNA BOTANOVA, QUINN LATIMER',
-			cover: 'book_3.jpg'
+			cover: 'book_3.jpeg'
 		},
 		{
 
 			title: 'Parapolitics',
 			writers: 'ANSELM FRANKE, NIDA GHOUSE, PAZ GUEVARA, ANTONIA MAJACA (EDS.)',
-			cover: 'book_4.jpg'
+			cover: 'book_4.jpeg'
 		},
 		{
 			title: 'Visual Cultures as Time Travel',
 			writers: 'HENRIETTE GUNKEL, AYESHA HAMEED',
-			cover: 'book_5.jpg'
+			cover: 'book_5.jpeg'
 		},
 		{
 			title: 'Dear Navigator',
 			writers: 'HU FANG',
-			cover: 'book_6.jpg'
+			cover: 'book_6.jpeg'
 		},
 	],
 
@@ -107,26 +107,26 @@ export default {
 			title: 'And My Flames Stay Till You Get Out My Way',
 			artist: 'April Bey',
 			location: 'Museum für Moderne Kunst',
-			image: 'journal_1.jpg'
+			image: 'journal_1.jpeg'
 		},
 		{
 			title: 'Crip Time',
 			artist: 'Kenny Fries',
 			location: 'Vienna Art Week',
-			image: 'journal_2.jpg'
+			image: 'journal_2.jpeg'
 		},
 		{
 			title: 'Losing Control',
 			artist: 'Alte Pinakothek',
 			location: 'Novuyo Moyo',
-			image: 'journal_3.jpg'
+			image: 'journal_3.jpeg'
 		},
 	],
 
 	pages: [
 		{
 			name: 'About us',
-			slug: 'about-us'
+			slug: 'about-us',
 			content: {
 				title: 'in(dex) is the largest repository of writing in the arts',
 				body: [
--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
-
 import App from './App.vue';
-
 import routes from '../routes.js';
 
 const app_router = createRouter({
