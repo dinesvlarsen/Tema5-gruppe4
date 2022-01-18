20c93d07d4c5da956cf646f50a8a24027543f9d6
Guro Walle <guro.terjesdatter.walle@gmail.com>
2022-01-18 13:55:57 +0100

    Finished styling and scripting LandigSite.vue in the components folder. Implemented it in Home.vue Made LandingSite.vue in the views folder and implemented everything needed there.

---
+++ src/views/LandingSite.vue
@@ -0,0 +1,15 @@
+<template>
+  <LandingSite />
+</template>
+
+<style></style>
+
+<script>
+import LandingSite from "../components/LandingSite.vue";
+
+export default {
+  components: {
+    LandingSite,
+  },
+};
+</script>
