e5f297711d6689e3ef1560599b32dff92c14d872
Dines <dinesvlarsen@gmail.com>
2022-01-19 16:55:03 +0100

    added journal slideshow components

---
+++ src/components/JournalSlideshow.vue
@@ -0,0 +1,9 @@
+<template>
+	<h1>Slideshow</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/JournalSlideshow.vue
@@ -0,0 +1,12 @@
+<template>
+	<JournalSlideshow />
+</template>
+
+<script>
+import JournalSlideshow from '../components/JournalSlideshow.vue';
+export default {
+	components: JournalSlideshow,
+};
+</script>
+
+<style></style>
