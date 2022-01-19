32a6775363b3627e940d9e4acec689228c1aa349
Dines <dinesvlarsen@gmail.com>
2022-01-19 16:38:28 +0100

    removed mainarticles route and added a route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import Home from './views/Home.vue';
-import MainArticles from './views/MainArticles.vue';
 import ArticleMutualAid from './views/ArticleMutualAid.vue';
+
+//Only adding this route while developing the component, not needed for deployment
+import JournalSlideshow from './views/JournalSlideshow.vue';
 
 export default [
 	{ name: 'home', path: '/', component: Home },
-	{ name: 'mainarticles', path: '/', props: true, component: MainArticles },
-	{ name: 'Article-mutual-aid', path: '/mutual-aid-social-distancing-and-dual-power-in-the-state-of-emergency', component: ArticleMutualAid }
+	{ name: 'Article-mutual-aid', path: '/mutual-aid-social-distancing-and-dual-power-in-the-state-of-emergency', component: ArticleMutualAid }, { name: 'journal-slideshow', path: '/journal-slideshow', component: JournalSlideshow }
 ];
