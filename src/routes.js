733bab45faf16761867f60b5e9195a2c95bc1b7e
Dines <dinesvlarsen@gmail.com>
2022-01-19 17:30:47 +0100

    removed unecessary views files and links

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import Home from './views/Home.vue';
 import ArticleMutualAid from './views/ArticleMutualAid.vue';
 
+
 export default [
 	{ name: 'home', path: '/', component: Home },
 	{ name: 'Article-mutual-aid', path: '/mutual-aid-social-distancing-and-dual-power-in-the-state-of-emergency', component: ArticleMutualAid }
