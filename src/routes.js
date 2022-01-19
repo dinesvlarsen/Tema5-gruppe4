eac9f6f6b3a7757a3cdb891b2b5ec3e80f8975b0
Dines <dinesvlarsen@gmail.com>
2022-01-19 17:07:24 +0100

    Removed slideshow routes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,7 @@
 import Home from './views/Home.vue';
 import ArticleMutualAid from './views/ArticleMutualAid.vue';
 
-//Only adding this route while developing the component, not needed for deployment
-import JournalSlideshow from './views/JournalSlideshow.vue';
-
 export default [
 	{ name: 'home', path: '/', component: Home },
-	{ name: 'Article-mutual-aid', path: '/mutual-aid-social-distancing-and-dual-power-in-the-state-of-emergency', component: ArticleMutualAid }, { name: 'journal-slideshow', path: '/journal-slideshow', component: JournalSlideshow }
+	{ name: 'Article-mutual-aid', path: '/mutual-aid-social-distancing-and-dual-power-in-the-state-of-emergency', component: ArticleMutualAid }
 ];
