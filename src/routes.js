5fcf56288fc8f3a49eacffa0e262c105180ca8e2
Dines <dinesvlarsen@gmail.com>
2022-01-19 14:44:30 +0100

    changed path to mainarticles

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,6 @@ import ArticleMutualAid from './views/ArticleMutualAid.vue';
 
 export default [
 	{ name: 'home', path: '/', component: Home },
-	{ name: 'mainarticles', path: '', props: true, component: MainArticles },
+	{ name: 'mainarticles', path: '/', props: true, component: MainArticles },
 	{ name: 'Article-mutual-aid', path: '/mutual-aid-social-distancing-and-dual-power-in-the-state-of-emergency', component: ArticleMutualAid }
 ];
