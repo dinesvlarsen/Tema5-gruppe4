baaa11c5b4b189e56dba0cc78f842e684491a063
Dines <dinesvlarsen@gmail.com>
2022-01-20 14:37:18 +0100

    removed code that wasnt in use

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,7 @@
 			:lead="getLead(1)"
 			:image="getImage(1)"
 		/>
+
 		<!-- This includes the lead and image in the link. Not sure if this should be split up or if RouterLink should be set up on component with a dynamic name (ie :slug from the database) -->
 		<RouterLink :to="{ name: 'article-mutual-aid' }">
 			<MainArticles
@@ -26,11 +27,9 @@ import LandingSite from '../components/LandingSite.vue';
 import MainArticles from '../components/MainArticles.vue';
 import JournalSlideshow from '../components/JournalSlideshow.vue';
 
-
 export default {
 	data() {
 		return {
-    
 			//Stores all the data imported from database.js into the data property.
 			data: prototypeData,
 		};
@@ -41,7 +40,6 @@ export default {
 		getTitle(index) {
 			return this.data.articles[index].title;
 		},
-
 
 		getSlug(index) {
 			return this.data.articles[index].slug;
@@ -54,37 +52,18 @@ export default {
 			return this.data.articles[index].preview.image;
 		},
 
-
 		getArticle(index) {
 			return this.data.articles[index].body[0];
 		},
-
-		//Might not need this because I use data inside of slideshow...!!!!!!!
-		// //Gets journal data you need based on index and data.
-		// getJournalTitle(index) {
-		// 	return this.data.journal[index].title;
-		// },
-
-		// getJournalArtist(index) {
-		// 	return this.data.journal[index].artist;
-		// },
-
-		// getJournalLocation(index) {
-		// 	return this.data.journal[index].location;
-		// },
-
-		// getJournalImage(index) {
-		// 	return this.data.journal[index].image;
-		// },
 
 		getImageCaption(index) {
 			return this.data.articles[index].preview.caption;
 		},
+
 		//Gets the body of an article by specifying which article you want (see line 30)
 		getArticle(index) {
 			return this.data.articles[index].body[0];
 		},
-
 	},
 
 	components: {
@@ -92,7 +71,6 @@ export default {
 		MainArticles,
 
 		JournalSlideshow,
-
 	},
 };
 </script>
