02b89db27026b37884f62a67a68890831ddcbd3f
Dines <dinesvlarsen@gmail.com>
2022-01-20 19:22:36 +0100

    made books component

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -1,54 +1,95 @@
 <template>
-  <section class="books">
-    <p class="books__title">Books</p>
-    <div v-for="bla in books" class="books__grid">
-      <div class="grid__items">
-        <img src="../../assets/images/book_6.jpeg" alt="" />
-        <p>{{ title }}</p>
-        <p>{{ writers }}</p>
-      </div>
-    </div>
-  </section>
+	<section class="books">
+		<h2 class="books__heading">Books</h2>
+		<div class="library">
+			<div class="book" v-for="(book, index) in booksList" :key="book.title">
+				<img
+					class="book__cover"
+					:src="`../../assets/images/${book.cover}`"
+					:alt="`Picture of the book ${book.title}`"
+				/>
+				<h3 class="book__title">{{ titleCase(book.title) }}</h3>
+				<p class="book__writers">by {{ upperCaseText(book.writers) }}</p>
+			</div>
+		</div>
+	</section>
 </template>
 
-
 <script>
-import prototypeData from "../../assets/database.js";
+import Data from './../../assets/database.js';
 
 export default {
-  props: ["title", "writers", "cover"],
+	data() {
+		return {
+			booksList: Data.books,
+		};
+	},
 
-  data() {
-    return {
-      books: {
-        title: prototypeData.books.title,
-        writers: prototypeData.books.writers,
-        cover: prototypeData.books.cover,
-      },
-    };
-  },
-  computed: {
-    imageLink() {},
-  },
+	methods: {
+		upperCaseText(string) {
+			return string.toUpperCase();
+		},
+
+		titleCase(string) {
+			string = string
+				.toLowerCase()
+				.split(' ')
+				.map((word) => {
+					return word.charAt(0).toUpperCase() + word.slice(1);
+				});
+			return string.join(' ');
+		},
+	},
 };
 </script>
 
 <style>
-.books__title {
-  margin: 3em auto 0.5em;
-  text-align: center;
-  font-size: 30px;
+.books {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin-top: 3.75rem;
 }
 
-.books__grid {
-  display: grid;
-  column-gap: 1rem;
-  row-gap: 100px;
-  margin: 0 1rem;
-  grid-template-columns: 1fr 1fr 1fr;
+.books__heading {
+	font-size: 1.85rem;
+	text-align: center;
+	margin-bottom: 2.5rem;
 }
 
-.grid__items {
-  display: grid;
+.library {
+	margin: 0 1rem;
+}
+
+.book {
+	max-width: 306px;
+	justify-content: center;
+	margin-bottom: 2.5rem;
+}
+
+.book__cover {
+	margin-bottom: 0.625rem;
+}
+.book__title {
+	font-size: 1.25rem;
+	margin-bottom: 0.5rem;
+}
+.book__writers {
+	font-size: 1rem;
+}
+
+@media screen and (min-width: 600px) {
+	.library {
+		display: grid;
+		max-width: 1920px;
+		grid-template-columns: repeat(3, 1fr);
+		gap: 10px;
+	}
+}
+
+@media screen and (min-width: 1175px) {
+	.library {
+		grid-template-columns: repeat(6, 1fr);
+	}
 }
 </style>
