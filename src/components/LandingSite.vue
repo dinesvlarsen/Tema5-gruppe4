c2c0af71931a6ef4fbe0fb2b9f12fe39ecacd105
Dines <dinesvlarsen@gmail.com>
2022-01-20 15:52:30 +0100

    added routerlinks

--- src/components/LandingSite.vue
+++ src/components/LandingSite.vue
@@ -2,7 +2,7 @@
 	<section class="landingSite">
 		<p class="landingSite__latest">Latest</p>
 		<!--Router link skal rundt h1 og p-->
-		<RouterLink :to="{ name: 'article-cosmos' }">
+		<RouterLink :to="routerLink">
 			<h1 class="landingSite__header">{{ title }}</h1>
 			<p class="landingSite__paragraph">
 				{{ lead }}
@@ -13,66 +13,70 @@
 </template>
 
 <script>
-import prototypeData from "../../assets/database.js";
+import prototypeData from '../../assets/database.js';
 
 export default {
-  props: ["title", "lead", "image"],
-  data() {
-    return {
-      firstArticle: {
-        title: prototypeData.articles[0].title,
-        lead: prototypeData.articles[0].lead,
-        body: prototypeData.articles[0].body,
-        slug: prototypeData.articles[0].slug,
-        preview: prototypeData.articles[0].preview,
-      },
-    };
-  },
+	props: ['title', 'lead', 'image', 'slug'],
+	data() {
+		return {
+			firstArticle: {
+				title: prototypeData.articles[0].title,
+				lead: prototypeData.articles[0].lead,
+				body: prototypeData.articles[0].body,
+				slug: prototypeData.articles[0].slug,
+				preview: prototypeData.articles[0].preview,
+			},
+		};
+	},
 
-  computed: {
-    imageLink() {
-      return "../../assets/images/article_1.jpeg";
-    },
-  },
+	computed: {
+		imageLink() {
+			return '../../assets/images/article_1.jpeg';
+		},
+
+		routerLink() {
+			return `/${this.slug}`;
+		},
+	},
 };
 </script>
 
 <style>
 .landingSite {
-  display: flex;
-  flex-direction: column;
-  text-align: center;
+	display: flex;
+	flex-direction: column;
+	text-align: center;
 }
 
 .landingSite__latest {
-  margin: 0.5em auto;
-  font-size: 30px;
+	margin: 0.5em auto;
+	font-size: 30px;
 }
 
 .landingSite__header {
-  margin: 0.5em auto;
-  font-size: 50px;
-  max-width: 27ch;
+	margin: 0.5em auto;
+	font-size: 50px;
+	max-width: 27ch;
 }
 
 .landingSite__paragraph {
-  margin: 0.5em 1em 0.5em;
-  font-size: 20px;
+	margin: 0.5em 1em 0.5em;
+	font-size: 20px;
 }
 
 .landingSite__image {
-  width: 96%;
-  margin: 0.5em auto;
+	width: 96%;
+	margin: 0.5em auto;
 }
 
 @media screen and (min-width: 968px) {
-  .landingSite__image {
-    width: 70%;
-  }
+	.landingSite__image {
+		width: 70%;
+	}
 
-  .landingSite__paragraph {
-    max-width: 60ch;
-    margin: 0.5em auto;
-  }
+	.landingSite__paragraph {
+		max-width: 60ch;
+		margin: 0.5em auto;
+	}
 }
 </style>
