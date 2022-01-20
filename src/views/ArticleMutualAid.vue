8fbd6ae6281121f04722875f4aa0d00607a6af26
Dines <dinesvlarsen@gmail.com>
2022-01-20 15:53:00 +0100

    added prettier formatting

--- src/views/ArticleMutualAid.vue
+++ src/views/ArticleMutualAid.vue
@@ -1,25 +1,31 @@
 <template>
-	<Article :title="database.title" :author="database.author" :body="database.body" :image="database.image" :caption="database.caption" />
+	<Article
+		:title="database.title"
+		:author="database.author"
+		:body="database.body"
+		:image="database.image"
+		:caption="database.caption"
+	/>
 </template>
 
 <script>
-	import Article from "../components/Article.vue";
-	import data from "../../assets/database.js";
+import Article from '../components/Article.vue';
+import data from '../../assets/database.js';
 
-	export default {
-		components: {
-			Article,
+export default {
+	components: {
+		Article,
 	},
 
 	data() {
 		return {
 			database: {
-					title: data.articles[2].title,
-					author: data.articles[2].author,
-					body: data.articles[2].body,
-					image: data.articles[2].preview.image,
-					caption: data.articles[2].preview.caption,
-				}
+				title: data.articles[2].title,
+				author: data.articles[2].author,
+				body: data.articles[2].body,
+				image: data.articles[2].preview.image,
+				caption: data.articles[2].preview.caption,
+			},
 		};
 	},
 };
