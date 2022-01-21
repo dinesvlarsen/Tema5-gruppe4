f74d53f6e391adea41ac5c2fa9296d899b7b2bc1
reiv003 <reiv003@student.kristiania.no>
2022-01-21 17:53:05 +0100

    Changed article body properties in database from array to single string

--- src/routes.js
+++ src/routes.js
@@ -4,15 +4,15 @@ import ArticleMutualAid from './views/ArticleMutualAid.vue';
 import ArticleCosmos from './views/ArticleCosmos.vue';
 import ArticleJimmieDurham from './views/ArticleJimmieDurham.vue';
 import Footer from "./components/Footer.vue";
-  
+
 import Data from './../assets/database.js';
 const data = Data;
 
 
 export default [
 	{ name: 'home', path: '/', component: Home },
-	{ name: 'article-mutual-aid', path: `/${data.articles[0].slug}`, component: ArticleMutualAid },
+	{ name: 'article-mutual-aid', path: `/${data.articles[2].slug}`, component: ArticleMutualAid },
 	{ name: 'article-jimmie-durham', path: `/${data.articles[1].slug}`, component: ArticleJimmieDurham },
-	{ name: 'article-cosmos', path: `/${data.articles[2].slug}`, component: ArticleCosmos },
-  { name: "footer", path: "/footer", component: Footer },
+	{ name: 'article-cosmos', path: `/${data.articles[0].slug}`, component: ArticleCosmos },
+	{ name: "footer", path: "/footer", component: Footer },
 ];
