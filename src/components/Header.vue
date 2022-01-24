d22fe41a5b77a051551c0f35a061aa020cad5851
hanneols1 <hanne.eline.olsen@gmail.com>
2022-01-24 12:42:09 +0100

    Updatet header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,63 @@
 <template>
-	<h1>Header</h1>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+  </head>
+  <body>
+    <div class="topnav">
+      <a href="#home" class="active">In(dex)</a>
+      <div id="myLinks">
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+      </div>
+      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+      </a>
+    </div>
+  </body>
 </template>
+
+<style>
+
+
+.topnav {
+  overflow: hidden;
+  background-color: white;
+  position: relative;
+}
+
+.topnav #myLinks {
+  display: none;
+}
+
+.topnav a {
+  color: black;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+  display: block;
+}
+
+.topnav a.icon {
+  display: block;
+  position: absolute;
+  right: 0;
+  top: 0;
+}
+</style>
+
+
+<script>
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,68 +1,77 @@
 <template>
-	<LandingSite
-		:title="getArticleData(0, 'title')"
-		:lead="getArticleData(0, 'lead')"
-		:image="getArticleData(0, 'image')"
-		:slug="getArticleData(0, 'slug')"
-	/>
+  <Header
+    :title="getArticleData(0, 'title')"
+    :lead="getArticleData(0, 'lead')"
+    :image="getArticleData(0, 'image')"
+    :slug="getArticleData(0, 'slug')"
+  />
 
-	<div class="divider">
-		<MainArticles
-			:title="getArticleData(1, 'title')"
-			:lead="getArticleData(1, 'lead')"
-			:image="getArticleData(1, 'image')"
-			:slug="getArticleData(1, 'slug')"
-		/>
+  <LandingSite
+    :title="getArticleData(0, 'title')"
+    :lead="getArticleData(0, 'lead')"
+    :image="getArticleData(0, 'image')"
+    :slug="getArticleData(0, 'slug')"
+  />
 
-		<!-- This includes the lead and image in the link. Not sure if this should be split up or if RouterLink should be set up on component with a dynamic name (ie :slug from the database) -->
-		<MainArticles
-			:title="getArticleData(2, 'title')"
-			:lead="getArticleData(2, 'lead')"
-			:image="getArticleData(2, 'image')"
-			:slug="getArticleData(2, 'slug')"
-		/>
-	</div>
+  <div class="divider">
+    <MainArticles
+      :title="getArticleData(1, 'title')"
+      :lead="getArticleData(1, 'lead')"
+      :image="getArticleData(1, 'image')"
+      :slug="getArticleData(1, 'slug')"
+    />
 
-	<Books />
+    <!-- This includes the lead and image in the link. Not sure if this should be split up or if RouterLink should be set up on component with a dynamic name (ie :slug from the database) -->
+    <MainArticles
+      :title="getArticleData(2, 'title')"
+      :lead="getArticleData(2, 'lead')"
+      :image="getArticleData(2, 'image')"
+      :slug="getArticleData(2, 'slug')"
+    />
+  </div>
 
-	<JournalSlideshow />
+  <Books />
+
+  <JournalSlideshow />
 </template>
 
 <script>
-import prototypeData from '../../assets/database.js';
-import LandingSite from '../components/LandingSite.vue';
-import MainArticles from '../components/MainArticles.vue';
-import Books from '../components/Books.vue';
-import JournalSlideshow from '../components/JournalSlideshow.vue';
+import prototypeData from "../../assets/database.js";
+import LandingSite from "../components/LandingSite.vue";
+import MainArticles from "../components/MainArticles.vue";
+import Books from "../components/Books.vue";
+import JournalSlideshow from "../components/JournalSlideshow.vue";
+import Header from "../components/Header.vue";
 
 export default {
-	data() {
-		return {
-			//Stores all the data imported from database.js into the data property.
-			data: prototypeData,
-		};
-	},
+  data() {
+    return {
+      //Stores all the data imported from database.js into the data property.
+      data: prototypeData,
+    };
+  },
 
-	methods: {
-		//Can use index and  key to access articles from database
-		//key expects a string with one of these:('title', 'slug', 'lead', 'body', 'image', 'caption')
-		getArticleData(index, key) {
-			//Conditionals are here to check if 'image' or 'caption', since they are in their own object.
-			if (key === 'image') {
-				return this.data.articles[index].preview.image;
-			} else if (key === 'caption') {
-				return this.data.articles[index].preview.caption;
-			}
+  methods: {
+    //Can use index and  key to access articles from database
+    //key expects a string with one of these:('title', 'slug', 'lead', 'body', 'image', 'caption')
+    getArticleData(index, key) {
+      //Conditionals are here to check if 'image' or 'caption', since they are in their own object.
+      if (key === "image") {
+        return this.data.articles[index].preview.image;
+      } else if (key === "caption") {
+        return this.data.articles[index].preview.caption;
+      }
 
-			return this.data.articles[index][key];
-		},
-	},
+      return this.data.articles[index][key];
+    },
+  },
 
-	components: {
-		LandingSite,
-		MainArticles,
-		Books,
-		JournalSlideshow,
-	},
+  components: {
+    LandingSite,
+    MainArticles,
+    Books,
+    JournalSlideshow,
+    Header,
+  },
 };
 </script>
