68baa196bd63b0aa458a99302e327e2dd30080da
Dines <dinesvlarsen@gmail.com>
2022-01-25 12:45:39 +0100

    refactored a bit of code

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,39 +1,47 @@
 <template>
 	<div class="article-container">
 		<div>{{ author }}</div>
-		<h1> {{ title }}  </h1>
-			<figure>
-				<img :src="'../../assets/images/' + `${image}`" />
-				<figcaption> {{ caption }} </figcaption>
-				<br><br>
-			</figure>
-		<div> {{ body }} </div>
+		<h1>{{ title }}</h1>
+		<figure>
+			<img :src="'../../assets/images/' + `${image}`" />
+			<figcaption>{{ caption }}</figcaption>
+			<br /><br />
+		</figure>
+		<article
+			class="article-container__article"
+			v-for="article in body"
+			:key="article"
+		>
+			{{ article }}
+		</article>
+		<!-- <div>{{ body }}</div> -->
 	</div>
 </template>
 
 <script>
-	export default {
-		props: ['title', 'author', 'body', 'image', 'caption'],
+export default {
+	props: ['title', 'author', 'body', 'image', 'caption'],
 
-		data() {
-			return {
-				}
-			}
-	}
+	data() {
+		return {};
+	},
+};
 </script>
 
 <style>
-
+.article-container__article {
+	margin-bottom: 50px;
+}
 .article-container {
 	white-space: pre-line;
-	}
+}
 
-.article-container div, .article-container figcaption {
+.article-container div,
+.article-container figcaption {
 	margin: var(--margin);
-}	
+}
 
 .article-container h1 {
 	margin: 0.1em;
 }
-
 </style>
