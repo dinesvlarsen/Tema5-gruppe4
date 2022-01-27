957660bdaad9ecc2f9959cb16899013e8dc26fcd
reiv003 <reiv003@student.kristiania.no>
2022-01-27 16:07:01 +0100

    Added functionality and styling for displaying email input field.

--- src/components/Newsletter.vue
+++ src/components/Newsletter.vue
@@ -1,22 +1,30 @@
 <template>
 <!-- If the value "promptOpen" is true, the newsletter element will be present in the DOM, as it is by default. 
 Clicking the X within the element changes the value to false, removing the element from the DOM and hiding it from view. -->
-	<div v-if="promptOpen" class="newsletterPrompt">
-		<div class="newsletterText">{{ message }}</div>
-			<div>
-				<button @click="promptOpen = false" aria-label="Close newsletter prompt"><img src="/assets/images/cross-icon.svg"/></button>
-			</div>
-	</div>
+		<div v-if="promptOpen" class="newsletterPrompt">
+				<div class="newsletter__text">
+					<button @click="showInput = true"><div v-show="showInput === false">{{ message }}</div></button>
+					<div class="subscribeInput" v-if="showInput === true">
+						<label for="email">Enter your e-mail:</label>
+						<input type="email" id="email" name="email" placeholder="enter your e-mail">
+						<button type="button">subscribe</button>
+					</div>
+				</div>
+				<div>
+					<button @click="promptOpen = false" aria-label="Close newsletter prompt"><img src="/assets/images/cross-icon.svg"/></button>
+				</div>
+		</div>
 </template>
 
 <script>
-import data from "../../assets/database.js"
+import data from "../../assets/database.js";
 
 export default {
 	data() {
 		return {
 			message: data.newsletter.text,
-			promptOpen: true
+			promptOpen: true,
+			showInput: false
 		};
 	},
 }
@@ -24,7 +32,7 @@ export default {
 
 <style>
 
-/* A lot of unique values for padding etc that we might be able to remove by setting up a grid on Home.vue. (ie have it span the whole grid with some padding, like the figcaption on articles). For now the positioning is done on the component itself.  */
+/* A lot of unique values for padding etc that we might be able to remove by setting up a grid on Home.vue. (ie have it span the whole grid with some margin, like the figcaption on articles). For now the positioning is done on the component itself.  */
 .newsletterPrompt {
 	display: flex;
 	flex-flow: row;
@@ -44,13 +52,31 @@ export default {
 	padding: 5px;
 }
 
-.newsletterText {
-	flex: 1;
-	font-size: var(--body-font-size);
+.newsletterPrompt label {
+	visibility: hidden;
+}
+
+.newsletterPrompt input[type=email] {
+	border-bottom: 1px black solid;
 }
 
 .newsletterPrompt:hover {
 	background-color: var(--highlight-color);
+}
+
+.newsletter__text {
+	flex: 1;
+	font-size: var(--body-font-size);
+}
+
+.subscribeInput button {
+	background-color: var(--highlight-color);
+	border-radius: 10px;
+	margin-left: var(--margin);
+	padding-top: 2px;
+	padding-bottom: 2px;
+	padding-left: 70px;
+	padding-right: 70px;
 }
 
 @media (max-width: 1280px) {
