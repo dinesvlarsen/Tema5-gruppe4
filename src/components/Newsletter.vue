dce678749174f2b052b9e1425c3e84e432768cd7
reiv003 <reiv003@student.kristiania.no>
2022-01-28 00:29:03 +0100

    Made email input longer and added an alert to the subscribe button

--- src/components/Newsletter.vue
+++ src/components/Newsletter.vue
@@ -1,13 +1,15 @@
 <template>
 <!-- If the value "promptOpen" is true, the newsletter element will be present in the DOM, as it is by default. 
 Clicking the X within the element changes the value to false, removing the element from the DOM and hiding it from view. -->
-		<div v-if="promptOpen" class="newsletterPrompt">
+		<div v-if="promptOpen" class="newsLetterPrompt">
 				<div class="newsletter__text">
 					<button @click="showInput = true"><div v-show="showInput === false">{{ message }}</div></button>
 					<div class="subscribeInput" v-if="showInput === true">
-						<label for="email">Enter your e-mail:</label>
-						<input type="email" id="email" name="email" placeholder="enter your e-mail">
-						<button type="button">subscribe</button>
+						<form>
+							<label for="email">Enter your e-mail:</label>
+							<input type="email" v-model="readerEmail" id="email" name="email" placeholder="enter your e-mail">
+							<button type="submit" @click="subscribeResponse()">subscribe</button>
+						</form>
 					</div>
 				</div>
 				<div>
@@ -21,11 +23,23 @@ import data from "../../assets/database.js";
 
 export default {
 	data() {
+	
 		return {
 			message: data.newsletter.text,
 			promptOpen: true,
-			showInput: false
+			showInput: false,
+			readerEmail: '',
 		};
+	},
+
+	methods: {
+		subscribeResponse() {
+			event.preventDefault();
+			// alert(`E-mail registered: ${readerEmail}
+			
+			// Thank you, newsletters are coming soon!`)
+			alert('Thank you, newsletters are coming soon!');
+		},
 	},
 }
 </script>
@@ -42,7 +56,7 @@ export default {
 	border-radius: 10px;
 	z-index: 8;
 	position: fixed;
-	bottom: 0;
+	bottom: 10px;
 	left: 0;
 	width: 98%;
 	margin: 15px;
@@ -58,7 +72,12 @@ export default {
 
 .newsletterPrompt input[type=email] {
 	border-bottom: 1px black solid;
+	width: 623px;
 }
+
+/* .newsletterPrompt:not(.subscribeInput):hover {
+	background-color: blue;
+} */
 
 .newsletterPrompt:hover {
 	background-color: var(--highlight-color);
@@ -69,6 +88,12 @@ export default {
 	font-size: var(--body-font-size);
 }
 
+/* .subscribeInput {
+	z-index: 10;
+	background-color: var(--secondary-color);
+	width: 1700px;
+} */
+
 .subscribeInput button {
 	background-color: var(--highlight-color);
 	border-radius: 10px;
@@ -77,6 +102,10 @@ export default {
 	padding-bottom: 2px;
 	padding-left: 70px;
 	padding-right: 70px;
+}
+
+.subscribeInput button:hover {
+	font-weight: bold;
 }
 
 @media (max-width: 1280px) {
