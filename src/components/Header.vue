a95705113120933af9cfd9be1b0cbbfb6aee478f
Dines <dinesvlarsen@gmail.com>
2022-01-28 13:29:00 +0100

    Fixed stuff

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,29 @@
 <template>
 	<header class="header">
-		<div @click="toggle" class="hamburger__icon">
-			<img
-				src="../../assets/images/header-icons/Hamburger icon.svg"
-				alt="header"
-			/>
+		<div class="header__icons">
+			<!-- <div class="search__icon">
+        <img
+          src="../../assets/images/header-icons/Search icon.svg"
+          alt="header"
+        />
+      </div> -->
+			<RouterLink :to="'/'">
+				<div class="nav__logo">
+					<a href="#" class="active">in(dex)</a>
+				</div>
+			</RouterLink>
+
+			<div @click="toggle" class="hamburger__icon">
+				<img
+					src="../../assets/images/header-icons/Hamburger icon.svg"
+					alt="header"
+				/>
+			</div>
 		</div>
 
-		<!-- <div class="search__icon">
-			<img
-				src="../../assets/images/header-icons/Search icon.svg"
-				alt="header"
-			/>
-		</div> -->
-
-		<RouterLink :to="'/'">
-			<div class="nav__logo">
-				<a href="#" class="active">in(dex)</a>
-			</div>
-		</RouterLink>
-
-		<div class="nav" ref="collapsibleNav">
+		<div class="nav" :class="toggleNav ? 'hidden' : ''">
 			<div class="nav__collapsible">
-				<div class="nav__links" :class="toggleNav ? 'hidden' : ''">
+				<div class="nav__links">
 					<a href="#Announcements">Announcements</a>
 					<a href="#Journal">Journal</a>
 					<a href="#Architecture">Architecture</a>
@@ -34,7 +35,7 @@
 					<a href="#Podcasts">Podcasts</a>
 				</div>
 
-				<ul class="nav__socials" :class="toggleNav ? 'hidden' : ''">
+				<ul class="nav__socials">
 					<li>About us</li>
 					<br />
 					<li>Instagram ↗</li>
@@ -64,6 +65,69 @@ export default {
 
 <style>
 .header {
+	font-size: 30px;
+}
+
+.header__icons {
+	display: flex;
+	position: relative;
+	z-index: 99999999;
+	justify-content: space-between;
+	padding: var(--small-spacing);
+}
+
+.hamburger__icon {
+	align-self: center;
+}
+
+.nav {
+	position: absolute;
+	top: 0;
+	left: 0;
+	z-index: 999;
+	width: 100%;
+	text-align: center;
+	background: var(--secondary-color);
+}
+.hamburger__icon {
+	width: 32px;
+}
+
+.nav__links {
+	display: flex;
+	flex-direction: column;
+	font-size: var(--heading-font-size);
+	padding: 4rem 0;
+}
+
+.nav__socials {
+	display: none;
+}
+
+.hidden {
+	display: none;
+}
+
+@media screen and (min-width: 968px) {
+	.nav {
+	}
+	.nav__collapsible {
+		display: grid;
+		grid-template-columns: repeat(12, 1fr);
+	}
+	.nav__links {
+		font-size: var(--body-font-size);
+		grid-column-start: 7;
+		padding: 0;
+	}
+
+	.nav__socials {
+		display: block;
+		grid-column-start: 9;
+	}
+}
+/* .header {
+	display: flex;
 	position: sticky;
 	z-index: 9;
 	top: 0;
@@ -140,26 +204,29 @@ export default {
 	margin: 0 auto;
 } */
 
-.slide__nav ul {
+/* .slide__nav ul {
 	max-width: 100%;
 }
 
 .hidden {
 	display: none;
-}
+} */
 
-@media screen and (max-width: 968px) {
+/* @media screen and (max-width: 968px) {
 	.nav-collapsible {
 		display: block;
 		grid-template-columns: repeat(1, 1fr);
 	}
 	.nav__links {
 		font-size: var(--heading-font-size);
-		padding: var(--medium-spacing) 0;
+		padding: 4rem 0;
 		text-align: center;
 	}
 	.nav__socials {
 		display: none;
 	}
-}
+} */
+
+/* @media screen and (max-width: 630px) {
+}  */
 </style>
--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,23 +1,58 @@
 <template>
-  <AboutUs :title="database.title" :body="database.body" />
+	<section class="about-us">
+		<h2 class="about-us__title">{{ database.title }}</h2>
+
+		<p
+			class="about-us__body"
+			v-for="paragraph in database.body"
+			:key="paragraph"
+		>
+			{{ paragraph }}
+		</p>
+	</section>
 </template>
 
 <script>
-import AboutUs from "../components/AboutUs.vue";
-import data from "../../assets/database.js";
+import data from '../../assets/database.js';
 
 export default {
-  components: {
-    AboutUs,
-  },
-
-  data() {
-    return {
-      database: {
-        title: data.pages[0].content.title,
-        body: data.pages[0].content.body,
-      },
-    };
-  },
+	data() {
+		return {
+			database: {
+				title: data.pages[0].content.title,
+				body: data.pages[0].content.body,
+			},
+		};
+	},
 };
 </script>
+
+<style>
+.about-us__title {
+	text-align: center;
+	margin: 1em auto;
+	max-width: 22ch;
+}
+
+.about-us__body {
+	margin: var(--margin);
+	white-space: pre-line;
+}
+
+.about-us__body + .about-us__body {
+	margin-top: 50px;
+}
+
+@media screen and (min-width: 968px) {
+	.about-us__title {
+		/*max-width: 70%;*/
+		max-width: 36ch;
+	}
+
+	.about-us__body {
+		max-width: 70ch;
+		/* max-width: 70%; */
+		margin: 0 auto;
+	}
+}
+</style>
