8d4d447dc8ea6a326a07fcc52eef6ab50359a73a
reiv003 <reiv003@student.kristiania.no>
2022-01-28 13:05:47 +0100

    Fixed scroll position issue

--- src/app/app.js
+++ src/app/app.js
@@ -5,6 +5,17 @@ import routes from '../routes.js';
 
 const app_router = createRouter({
 	routes: routes,
+	/*the recommended   return { x: 0, y: 0 } from 
+	https://router.vuejs.org/guide/advanced/scroll-behavior.html
+	doesn't seem to work. so using a workaround from this github issue.:
+	 https://github.com/vuejs/vue-router/issues/3451 */
+	scrollBehavior(to, from, savedPosition) {
+		if (savedPosition) {
+			return savedPosition
+		} else {
+			window.scrollTo(0, 0);
+		}
+	},
 	history: createWebHistory()
 })
 
